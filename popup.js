a9034fb0ae793c1a00792b48ccdd74890939db95
Gaurav Pote <107623748+gauravpote000@users.noreply.github.com>
2024-02-22 19:09:38 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,22 @@
+(()=>{
+	"use strict";
+	document.getElementById("downloadTableAsCSVButton").addEventListener("click", ()=>{
+		chrome.tabs.query({active: true, currentWindow: true}, (tabs)=> {
+			chrome.tabs.executeScript(tabs[0].id, {file: "downloadcsv.js", allFrames:true});
+		});
+		window.close();
+	});
+	chrome.storage.local.get(["encoding"], (result)=> {
+		if(result["encoding"] == "utf"){
+			document.getElementById("encodingUTF8").checked = true;
+		}else{
+			document.getElementById("encodingUTF8BOM").checked = true;
+		}
+	});
+
+	
+	document.getElementById("encodingUTF8").addEventListener("click", ()=>{
+		chrome.storage.local.set({"encoding": "utf"});
+	});
+})(); 
+
